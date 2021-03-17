716eb7030bc0cdf528d005f290a7d5966c90654f
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2021-03-17 20:47:49 -0300

    refactor(components): fixed Banner UI

--- components/numbanner.js
+++ components/numbanner.js
@@ -1,18 +1,17 @@
-import styles from './Banner.module.css'
+import styles from "./Banner.module.css";
 
-export default function Banner({days}) {
-    const  each = days.toString().split('')
-    return (
-        <div>
-            <div className={styles.www}>
-                {
-                    each.map((number, index) => (
-                        <div  key={index} className={styles.placa}>
-                            {number}
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
-    )
+export default function Banner({ days }) {
+  const numbers = String(days).split("");
+
+  return (
+    <div>
+      <div className={styles.www}>
+        {numbers.map((number, index) => (
+          <div key={index} className={styles.placa}>
+            {number}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
